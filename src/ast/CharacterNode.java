31eaf59f9e64ff4365714a5bfadf2d94ad69a6ab
fuxiuss <13858209+fuxiuss@user.noreply.gitee.com>
2024-11-24 21:36:46 +0800

    处理转义字符

--- src/ast/CharacterNode.java
+++ src/ast/CharacterNode.java
@@ -33,7 +33,27 @@ public class CharacterNode {
      * @return ASCII值，字符常量在ASCII表中对应的整数值。
      */
     public int evaluate() {
-        // 从字符常量中获取第一个字符，并返回其 ASCII 值
+        // 从字符常量中获取第一个字符，并返回其 ASCII 值,并特殊处理转义字符
+        if (charConst.charAt(1) == '\\') {
+            switch (charConst.charAt(2)) {
+                case '0':
+                    return 0;
+                case 'n':
+                    return 10;
+                case 'r':
+                    return 13;
+                case 't':
+                    return 9;
+                case '\\':
+                    return 92;
+                case '\'':
+                    return 39;
+                case '\"':
+                    return 34;
+                default:
+                    return charConst.charAt(2);
+            }
+        }
         return charConst.charAt(1);
     }
 }
